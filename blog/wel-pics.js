8fc552b4a6b8b830767bb52714707cd1933b1c73
liceses <1801203413@qq.com>
2026-01-09 17:31:09 +0800

    feat:add arrow-style switch of the pictures of the welcome column

--- blog/wel-pics.js
+++ blog/wel-pics.js
@@ -4,8 +4,11 @@ var pics = [
   "resource/wel1.png","resource/wel2.png","resource/wel3.jpg","resource/wel4.jpg","resource/wel5.png"
 ];//图片数组
 
-//创建按钮组
+
 if(welcomeConfig.welpicdisplay == 'dot'){
+    //圆点模式
+    console.log("Dot mode");
+    //创建按钮组
     var swi_buts = [document.createElement('div'),document.createElement('div'),document.createElement('div'),document.createElement('div'),document.createElement('div')];
     swi_buts.forEach(function(element, index){
     element.className = 'pic-s-buttons';
@@ -46,7 +49,52 @@ if(welcomeConfig.welpicdisplay == 'dot'){
 
 });
 }else if(welcomeConfig.welpicdisplay == 'arrow'){
-    //箭头模式未实现
+    //箭头模式
+    console.log("Arrow mode");
+    var swi_arrows = [document.createElement('img'),document.createElement('img')]; //创建左右箭头元素
+    //设置箭头样式
+    swi_arrows[0].src = 'https://icons.iconarchive.com/icons/iconsmind/outline/128/Arrow-Left-2-icon.png';
+    swi_arrows[1].src = 'https://icons.iconarchive.com/icons/iconsmind/outline/128/Arrow-Right-2-icon.png';
+    swi_arrows[0].style.animationName = 'arrow-breathing_left';
+    swi_arrows[1].style.animationName = 'arrow-breathing_right';
+    swi_arrows.forEach(function(element, index){
+        //设置箭头样式
+        element.className = 'pic-s-arrows';
+        element.style.width = '48px';
+        element.style.height = '36px';
+        element.style.position = 'relative';
+        element.style.margin = '0 auto';
+        element.style.top = '50%';
+        element.style.opacity = '0.7';
+        element.style.filter = 'invert(100%)';
+        //element.style.transform = 'translateY(-50%)';
+        element.style.cursor = 'pointer';
+
+        //设置箭头动画
+        element.style.animationDuration = '2s';
+        element.style.animationIterationCount = 'infinite';
+        element.style.transition = 'opacity 0.3s ease-in-out';
+
+        //箭头悬停效果
+        element.addEventListener("mouseover", function() {
+            i_arrow = swi_arrows.indexOf(element);
+            swi_arrows[1 - i_arrow].style.animationPlayState = 'paused'; //暂停另一个箭头的动画
+            element.style.animationPlayState = 'paused';
+            element.style.opacity = '1';
+            element.style.transform = 'scale(1.5)';
+        });
+        //箭头移出效果
+        element.addEventListener("mouseout", function() {
+            i_arrow = swi_arrows.indexOf(element);
+            swi_arrows[1 - i_arrow].style.animationPlayState = 'running'; //暂停另一个箭头的动画
+            element.style.animationPlayState = 'running';
+            element.style.opacity = '0.7';
+            element.style.transform = 'scale(1)';
+        });
+
+        //将箭头添加到按钮组容器中
+        document.getElementsByClassName('pic-switch')[0].appendChild(element);
+    });
 }
 // var swi_but =document.getElementsByClassName("pic-s-buttons"); //获取按钮组
 
@@ -66,6 +114,7 @@ pic_mid.className = 'pic-mid';
 
 
 //初始化图片元素样式
+{
 pic_mid.style.height = '100vh';
 pic_mid.style.backgroundSize = 'cover';
 pic_mid.style.backgroundPosition = 'center';
@@ -93,13 +142,19 @@ pic_back.style.width = '100%';
 pic_mid.style.transition = 'opacity 0.3s ease-in-out';
 pic_front.style.transition = 'opacity 0.3s ease-in-out';
 pic_back.style.transition = 'opacity 0.3s ease-in-out';
+pic_mid.style.zIndex = '3';
+pic_front.style.zIndex = '2';
+pic_back.style.zIndex = '1';
+}
 //设置初始图片 
+{
+pic_front.style.backgroundImage = 'url(' + pics[(active_index - 1 + pics.length) % pics.length] + ')';
 pic_mid.style.backgroundImage = 'url(' + pics[active_index] + ')';
 pic_back.style.backgroundImage = 'url(' + pics[(active_index + 1) % pics.length] + ')';
 pic_element.appendChild(pic_back);
 pic_element.appendChild(pic_front);
 pic_element.appendChild(pic_mid);
-
+}
 
 //自动轮播功能
 function autoSlideDot() {
@@ -137,6 +192,32 @@ function autoSlideDot() {
   return autoSlideInterval;
 }
 
+function autoSlideArrow() {
+    setInterval(function(){
+       if (ifonclick === 1) {
+           ifonclick = 0;
+       }else{
+            active_index = (active_index + 1) % pics.length;
+
+        pic_mid.style.transition = 'opacity ' + welcomeConfig.welpictransition + 'ms ease-in-out';
+        pic_back.style.transition = 'opacity ' + welcomeConfig.welpictransition + 'ms ease-in-out';
+        pic_mid.offsetHeight; //触发重绘
+        pic_back.offsetHeight; //触发重绘  
+        pic_back.style.transition = 'none';
+        pic_back.style.opacity = '1';
+        pic_mid.style.opacity = '0';
+        setTimeout(() => {
+            pic_mid.style.transition = 'none';
+            pic_mid.style.backgroundImage = 'url(' + pics[active_index] + ')';
+            pic_mid.style.opacity = '1';
+            pic_back.style.opacity = '0';
+            pic_back.style.backgroundImage = 'url(' + pics[(active_index + 1) % pics.length] + ')';
+            pic_front.style.backgroundImage = 'url(' + pics[(active_index - 1 + pics.length) % pics.length] + ')';
+            ifonclick = 0;
+        }, welcomeConfig.welpictransition + 100);
+       }
+   }, welcomeConfig.welpicdelay);
+}
 
 if(welcomeConfig.welpicdisplay === "dot") {
     swi_buts[active_index].style.opacity = '1'; //设置初始按钮样式
@@ -144,32 +225,101 @@ if(welcomeConfig.welpicdisplay === "dot") {
     //按钮点击事件处理
     swi_buts.forEach(function(element, index){
     element.addEventListener('click', function(){
-        ifonclick = 1;
-        //clearInterval(autoSlideInterval); //点击按钮时清除自动轮播
-        index = Array.prototype.indexOf.call(swi_buts, this);
-        if(index === active_index) return; //点击当前按钮不做任何操作
-        swi_buts[active_index].style.opacity = '0.5'; //重置上一个按钮样式
-        swi_buts[index].style.opacity = '1';
-        pic_mid.style.opacity = '0';
-        pic_back.style.backgroundImage = 'url(' + pics[index] + ')';
-        pic_back.style.opacity = '1';
-        active_index = index;
-        setTimeout(function(){
-            pic_mid.style.transition = 'none';
-            pic_mid.style.backgroundImage = 'url(' + pics[active_index] + ')';
-            pic_mid.style.opacity = '1';
-            pic_back.style.opacity = '0';
+        if(ifonclick === 1) {setTimeout(function(){ifonclick = 0;}, 500); return;}//`防止多次点击
+        else{
+            ifonclick = 1;
+            index = Array.prototype.indexOf.call(swi_buts, this);
+            if(index === active_index) return; //点击当前按钮不做任何操作
+            swi_buts[active_index].style.opacity = '0.5'; //重置上一个按钮样式
+            swi_buts[index].style.opacity = '1';
+            pic_mid.style.opacity = '0';
+            pic_back.style.backgroundImage = 'url(' + pics[index] + ')';
+            pic_back.style.opacity = '1';
+            active_index = index;
+            setTimeout(function(){
+                pic_mid.style.transition = 'none';
+                pic_mid.style.backgroundImage = 'url(' + pics[active_index] + ')';
+                pic_mid.style.opacity = '1';
+                pic_back.style.opacity = '0';
+                ifonclick = 0;
+            }, 500);
+            pic_mid.style.transition = 'opacity 0.3s ease-in-out';
+            pic_mid.offsetHeight; //触发重绘
             
-        }, 1000);
-        pic_mid.style.transition = 'opacity 0.3s ease-in-out';
-        pic_mid.offsetHeight; //触发重绘
-        //autoSlide(); //重新启动自动轮播
-        return active_index, ifonclick;
+            //autoSlide(); //重新启动自动轮播
+            
+        }
+        
+        
+
     });
 });    
 }
 if(welcomeConfig.welpicdisplay === "arrow") {
-    //箭头模式未实现
+    autoSlideArrow();
+    swi_arrows[0].addEventListener('click', function(){
+        //左箭头点击事件处理
+        if(ifonclick === 1) return;
+        else{
+            ifonclick = 1;
+            
+        active_index = (active_index - 1 + pics.length) % pics.length;
+
+        pic_mid.style.transition = 'opacity ' + welcomeConfig.welpictransition + 'ms ease-in-out';
+        pic_front.style.transition = 'opacity ' + welcomeConfig.welpictransition + 'ms ease-in-out';
+        pic_mid.offsetHeight; //触发重绘
+        pic_front.offsetHeight; //触发重绘
+
+
+        pic_front.style.transition = 'none';
+        pic_front.style.opacity = '1';
+        pic_mid.style.opacity = '0';
+        setTimeout(() => {
+            pic_mid.style.transition = 'none';
+            pic_mid.style.backgroundImage = 'url(' + pics[active_index] + ')';
+            pic_mid.style.opacity = '1';
+
+            pic_front.style.opacity = '0';
+            pic_front.style.backgroundImage = 'url(' + pics[(active_index - 1 + pics.length) % pics.length] + ')';
+            pic_back.style.backgroundImage = 'url(' + pics[(active_index + 1) % pics.length] + ')';
+            ifonclick = 0;
+        }, welcomeConfig.welpictransition + 100);
+        
+        
+        }
+        
+
+        
+    });
+
+    swi_arrows[1].addEventListener('click', function(){
+        //右箭头点击事件处理
+        if(ifonclick === 1) return;
+        else{
+            ifonclick = 1;
+        active_index = (active_index + 1) % pics.length;
+
+        pic_mid.style.transition = 'opacity ' + welcomeConfig.welpictransition + 'ms ease-in-out';
+        pic_back.style.transition = 'opacity ' + welcomeConfig.welpictransition + 'ms ease-in-out';
+        pic_mid.offsetHeight; //触发重绘
+        pic_back.offsetHeight; //触发重绘  
+        pic_back.style.transition = 'none';
+        pic_back.style.opacity = '1';
+        pic_mid.style.opacity = '0';
+        setTimeout(() => {
+            pic_mid.style.transition = 'none';
+            pic_mid.style.backgroundImage = 'url(' + pics[active_index] + ')';
+            pic_mid.style.opacity = '1';
+            pic_back.style.opacity = '0';
+            pic_back.style.backgroundImage = 'url(' + pics[(active_index + 1) % pics.length] + ')';
+            pic_front.style.backgroundImage = 'url(' + pics[(active_index - 1 + pics.length) % pics.length] + ')';
+            ifonclick = 0;
+        }, welcomeConfig.welpictransition + 100);
+        
+        
+        }
+        
+    });
+    
 }
-//按钮点击事件绑定
 
