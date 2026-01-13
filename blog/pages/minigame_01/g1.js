b15df87602e73bfd57c2b9ed22341e794bae13c0
liceses <1801203413@qq.com>
2026-01-13 15:46:18 +0800

    fix :  a bug in minigame's background

--- blog/pages/minigame_01/g1.js
+++ blog/pages/minigame_01/g1.js
@@ -7,39 +7,60 @@ var pageWidth = window.innerWidth;
 var pageHeight = window.innerHeight;
 var textcos =[];
 var i_textco = 0;
+var textco_click = [];
+var i_textco_click = 0 ;
 var BackGround = document.getElementById('background');
 //settings
 {
     var time_falling = '10s';//下落的时间，根据下落速度和字符生成的高度计算得出。设定此值无效
     var dalay_falling = '50ms';//字符出现后在原高度停留的时间
-    var speed_falling = 100;//字符下落的速度，单位px/s
+    var speed_falling = 50;//字符下落的速度，单位px/s
     var num_fallingChars = 20;//无用变量，下落字符的数量不是确定的数字
-    var interval_falling = 10;//生成下落字符的周期(时间间隔)
+    var interval_falling = 50;//生成下落字符的周期(时间间隔)
 
 } 
+//更新窗口尺寸
 window.addEventListener('resize',function () {
     pageWidth = window.innerWidth;
     pageHeight = window.innerHeight;
 })
+//点击生成字符
+BackGround.addEventListener('click',function (event) {
+    let top_textco = event.clientY;
+    let left_textco = event.clientX;
+    falling_textcos(left_textco,top_textco);
+})
+
+
+
 //生成字母
 
 setInterval(() => {
-    textcos[i_textco]= document.createElement('div');
-    textcos[i_textco].classList.add('textco');
-    falling_textcos(textcos[i_textco]);
-    i_textco ++;
+falling_textcos();
 }, interval_falling);
 
-function falling_textcos(ele){
+function falling_textcos(x,y){
+    let ele = document.createElement('div');
+    ele.classList.add('textco');
     //在页面头部随机生成
     let r_left = (Math.floor((Math.random()*pageWidth)/16) -1) *16;
     //console.log(r_left);
     let r_top= Math.floor(Math.random() * pageHeight *0.9   + ele.offsetHeight);
     //console.log(r_top);
-    ele.style.left = r_left + 'px';
-    ele.style.top = r_top + 'px';
-    //下落
-    time_falling = (pageHeight - r_top)/speed_falling +'s';
+    if (x === undefined) {
+        ele.style.left = r_left + 'px';
+    }else{
+        ele.style.left = x + 'px';
+    }
+    if (y === undefined) {
+        ele.style.top = r_top + 'px';
+        //下落
+        time_falling = (pageHeight - r_top)/speed_falling +'s';
+    }else{
+        ele.style.top = y + 'px';
+        time_falling = (pageHeight - y)/speed_falling +'s';
+    }
+    
     //console.log(time_falling);
     ele.style.animationDuration = time_falling;
     BackGround.appendChild(ele);
